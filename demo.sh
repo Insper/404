6c1429c9d01e678a9833cc494b92ae0a9205c970
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-05-14 12:45:27 -0300

    script para executar a demo cor.py

---
+++ demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Certifique-se de que esta conectado na mesma rede que o robo, e que o IP esta atualizado no .bashrc"
+cd ~/catkin_ws/src/robot19/ros/exemplos_python/scripts
+gnome-terminal -e  "roscore"
+sleep  3
+gnome-terminal -e "roslaunch turtlebot3_bringup turtlebot3_remote.launch"
+sleep 5
+gnome-terminal -e  "rosrun topic_tools relay /raspicam_node/image/compressed /kamera"
+sleep 2
+gnome-terminal -e "rosrun exemplos_python cor.py"
+sleep 1
+gnome-terminal -e "rosrun rqt_reconfigure rqt_reconfigure"
+echo 0
