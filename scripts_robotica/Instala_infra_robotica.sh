9502a332cb2594fe3d947bd6505c708f6360a83f
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-09-09 14:14:09 -0300

    Update Instala_infra_robotica.sh

--- scripts_robotica/Instala_infra_robotica.sh
+++ scripts_robotica/Instala_infra_robotica.sh
@@ -53,18 +53,18 @@ then
    	  		   	               then
 								echo "os gits para controle do Turtlebot foram clonandos com sucesso dos repositórios da ROBOTIS"
 								cd $path
-								exec "./instalar_opencv_jupyter.sh"							
+								exec "~/404/scripts_robotica/instalar_opencv_jupyter.sh"							
 							
 								if [ $? -eq 0 ]
    	  		   	                	then
 									echo "o opencv no python3 foi instalado, vamos instalar no python2 agora"								cd $path
-									exec "./instalar_opencv_python2.sh"
+									exec "~/404/scripts_robotica//instalar_opencv_python2.sh"
 									
 									if [ $? -eq 0 ]
 	   	  		   	                	then
 										cd $path
 								 		echo "o opencv foi instalado com sucesso no python2 tambem, agora vamos instalar o hector slam"
-										exec "./instalar_hector_slam.sh"
+										exec "~/404/scripts_robotica//instalar_hector_slam.sh"
 								
 									fi
 								fi
