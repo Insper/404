123dcea695a82d23319cc3ec24061bdc0aa5649c
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-09-17 10:24:42 -0300

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 
-PATH=$HOME/404
-URL=https://github.com/Insper/404.git
 
 echo "Buscando por atualizações no git"
-    git -C $PATH stash
-    git -C $PATH checkout master
-    git -C $PATH pull origin master
+    git -C /home/borg/404 stash
+    git -C /home/borg/404 checkout master
+    git -C /home/borg/404 pull origin master
 
 sucess_update=0;
 
@@ -20,9 +18,9 @@ if [ "$sucess_update" == 0 ]; then
 
 
 log_update=$" Insira aqui a identificação da sua atualização"
-sucess_update=$date
-$log_update >> ~/404/update.txt
-$sucess_update >> ~/404/update.txt
+sucess_update=date
+echo $log_update >> ~/404/update.txt
+echo $sucess_update >> ~/404/update.txt
 
 
 fi
