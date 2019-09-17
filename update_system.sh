74753ada1d585a792eb7a07b9bec217995565dfb
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-09-17 09:49:47 -0300

    Create update_system.sh

---
+++ update_system.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sucess_update=0;
+
+if [ "$sucess_update" == 0 ]; then
+
+# insira aqui o seu Script para automatizar atualizações no SSD do aluno 
+
+
+
+log_update=$" Insira aqui a identificação da sua atualização"
+sucess_update=$date
+$log_update >> ~/404/update.txt
+$sucess_update >> ~/404/update.txt
