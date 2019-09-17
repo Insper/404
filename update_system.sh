2b9e2496002332c8ea7416bb9475e3de332e8d46
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-09-17 10:20:26 -0300

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+
+PATH=$HOME/404
+URL=https://github.com/Insper/404.git
+
+echo "Buscando por atualizações no git"
+    git -C $PATH stash
+    git -C $PATH checkout master
+    git -C $PATH pull origin master
+
 sucess_update=0;
 
+
 if [ "$sucess_update" == 0 ]; then
+
 
 # insira aqui o seu Script para automatizar atualizações no SSD do aluno 
 
@@ -12,3 +23,8 @@ log_update=$" Insira aqui a identificação da sua atualização"
 sucess_update=$date
 $log_update >> ~/404/update.txt
 $sucess_update >> ~/404/update.txt
+
+
+fi
+
+wait
