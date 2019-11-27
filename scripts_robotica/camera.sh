d79702e9b8d4edafc7a4fe6159a7389dd4381199
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-11-27 16:27:31 -0300

    Update camera.sh

--- scripts_robotica/camera.sh
+++ scripts_robotica/camera.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-# Author: Licia Sales
-# 25-Jun-19
-# rev2 - raspberry 4 - ubuntu 18.04
+
 while true
 do
     ip_host=$(rosnode machine)
-    echo $ip_host > /home/ubuntu/log.txt
-    awk '{print $1}'  /home/ubuntu/log.txt > /home/ubuntu/log2.txt
-    ip_host=$(cat /home/ubuntu/log2.txt)
     echo "aguardando ip"
+    echo "rosnode machine" $ip_host
+    echo $ip_host > /home/ubuntu/log.txt
+    host=$(awk '{print $1}'  /home/ubuntu/log.txt)
+    echo "host" $host
 
-    if [[ "$ip_host" =~ (([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$ ]]; then
-    echo "ip_host= " $ip_host
-    echo "Conectando com a camera"
-    gst-launch-1.0 -v v4l2src device=/dev/video0 ! 'video/x-h264,width=600,height=448,framerate=30/1' ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=$ip_host port=9000
-    break
+
+
+    if [[ "$host" =~ (([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$ ]]; then
+        echo "Ip do robo" $(hostname -I)
+        echo "O host pc ainda não criou o link de comunicação"
+
+        if [ $host != $(hostname -I) ]; then
+
+            echo "Conectado ao ip " $host
+            echo "Enviando os dados da camera..."
+            gst-launch-1.0 -v v4l2src device=/dev/video0 ! 'video/x-h264,width=600,height=448,framerate=30/1' ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=$host port=9000
+            break
+         fi
     fi
 done
 
