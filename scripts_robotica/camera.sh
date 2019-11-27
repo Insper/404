9d433608ca8506d8c2f2a087debff4f01073db08
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-11-27 15:26:12 -0300

    Update camera.sh

--- scripts_robotica/camera.sh
+++ scripts_robotica/camera.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # Author: Licia Sales
 # 25-Jun-19
-
-
+# rev2 - raspberry 4 - ubuntu 18.04
 while true
 do
-   ip_host=$(rosnode machine)
-   echo $ip_host > /home/pi/log.txt
-   host=$(cut -c16- /home/pi/log.txt)
-   echo "aguardando ip"
+    ip_host=$(rosnode machine)
+    echo $ip_host > /home/ubuntu/log.txt
+    awk '{print $1}'  /home/ubuntu/log.txt > /home/ubuntu/log2.txt
+    ip_host=$(cat /home/ubuntu/log2.txt)
+    echo "aguardando ip"
 
-    if [[ "$host" =~ (([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$ ]]; then
+    if [[ "$ip_host" =~ (([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$ ]]; then
+    echo "ip_host= " $ip_host
     echo "Conectando com a camera"
-    raspivid -n -w 640 -h 480 -b 1000000 -fps 30 -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=$host port=9000
+    gst-launch-1.0 -v v4l2src device=/dev/video0 ! 'video/x-h264,width=600,height=448,framerate=30/1' ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=$ip_host port=9000
     break
     fi
 done
+
 
 
