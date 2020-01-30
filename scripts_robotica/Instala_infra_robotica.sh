7dacf0efe039ccbff0d8a64227dc82784375fcd0
Licia Sales <40764768+liciascl@users.noreply.github.com>
2020-01-30 15:02:00 -0300

    Update Instala_infra_robotica.sh

--- scripts_robotica/Instala_infra_robotica.sh
+++ scripts_robotica/Instala_infra_robotica.sh
@@ -1,61 +1,138 @@
+######################################
+# INSTALL ROS ON UBUNTU              #
+######################################
 #!/bin/bash
 #Autor Lícia Sales
-#30-Jul-2019
+#30-Jan-2020
+
 
 path=$(pwd)
 echo $path
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
- sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main' | sudo tee /etc/apt/sources.list.d/realsense-public.list
 
-echo "as keys criptográficas que assinam o software do ROS foram configuradas com sucesso"
-sudo apt update
-sudo apt-get install -y ros-melodic-joy ros-melodic-teleop-tools  ros-melodic-teleop-twist-keyboard ros-melodic-laser-proc ros-melodic-rgbd-launch  ros-melodic-rosserial-arduino ros-melodic-rosserial-python ros-melodic-rosserial-server ros-melodic-rosserial-client ros-melodic-rosserial-msgs ros-melodic-amcl ros-melodic-map-server ros-melodic-move-base ros-melodic-urdf ros-melodic-xacro ros-melodic-compressed-image-transport ros-melodic-rqt-image-view  ros-melodic-navigation ros-melodic-interactive-markers  ros-melodic-kobuki-ftdi ros-melodic-ar-track-alvar-msgs ros-melodic-teleop-tools python-rosinstall
-echo "O ROS foi instalado com sucesso"
-sudo apt install -y python-rosinstall-generator ros-melodic-ar-track-alvar ros-melodic-ar-track-alvar-dbgsym ros-melodic-ar-track-alvar-msgs ros-melodic-desktop-full ros-melodic-eigen-conversions ros-melodic-eigen-conversions-dbgsym ros-melodic-eigen-stl-containers ros-melodic-gscam ros-melodic-opencv-apps ros-melodic-opencv-apps-dbgsym ros-melodic-parrot-arsdk ros-melodic-turtlebot3-applications-msgs ros-melodic-turtlebot3-fake ros-melodic-turtlebot3-fake-dbgsym ros-melodic-turtlebot3-gazebo ros-melodic-turtlebot3-gazebo-dbgsym ros-melodic-turtlebot3-msgs ros-melodic-turtlebot3-simulations ros-melodic-vision-opencv ros-melodic-turtlebot3-msgs ros-melodic-ar-track-alvar ros-melodic-turtlebot3-applications-msgs
-echo "As dependencias ROS do curso foram instaladas com sucesso"
-sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-* gstreamer1.0-libav* gstreamer1.0-plugins* 
-echo "Os softwares para lidar com stream de vídeo vindo do robô foram instaladas com sucesso"
-source /opt/ros/melodic/setup.bash
+# 1. SETUP YOUR SOURCES.LIST AND CONFIGURE THE KEYS
+
+
+echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main' | sudo tee /etc/apt/sources.list.d/realsense-public.list  #Key libRealSense2
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 #Key Ros
+
+
+
+# 2. KEEP UBUNTU OR DEBIAN UP TO DATE
+
+sudo apt-get -y update
+# sudo apt-get -y upgrade       # Uncomment to install new versions of packages currently installed
+# sudo apt-get -y dist-upgrade  # Uncomment to handle changing dependencies with new vers. of pack.
+# sudo apt-get -y autoremove    # Uncomment to remove packages that are now no longer needed
+
+
+# 3. INSTALL ROS DESKTOP-FULL
+
+sudo apt install -y ros-melodic-desktop-full
+
+# 4. DEPENDECIES FOR BUILDING PACKAGES
+
+# Build tools:
+sudo apt-get install -y  python-rosinstall-generator
+sudo apt-get install -y python-rosinstall 
+sudo apt-get install -y ros-melodic-joy
+sudo apt-get install -y ros-melodic-teleop-tools
+sudo apt-get install -y ros-melodic-teleop-twist-keyboard 
+sudo apt-get install -y ros-melodic-laser-proc 
+
+# Build Rosserial:
+sudo apt-get install -y ros-melodic-rosserial-arduino
+sudo apt-get install -y ros-melodic-rosserial-python
+sudo apt-get install -y ros-melodic-rosserial-server
+sudo apt-get install -y ros-melodic-rosserial-client
+sudo apt-get install -y ros-melodic-rosserial-msgs
+sudo apt-get install -y ros-melodic-amcl
+
+# Build Navigation:
+sudo apt-get install -y ros-melodic-openslam-gmapping
+sudo apt-get install -y ros-melodic-map-server
+sudo apt-get install -y ros-melodic-move-base
+sudo apt-get install -y ros-melodic-urdf 
+sudo apt-get install -y ros-melodic-xacro 
+sudo apt-get install -y ros-melodic-navigation 
+sudo apt-get install -y ros-melodic-interactive-markers 
+sudo apt-get install -y ros-melodic-kobuki-ftdi 
+
+# Build Image tools:
+sudo apt-get install -y ros-melodic-compressed-image-transport
+sudo apt-get install -y ros-melodic-rqt-image-view
+sudo apt-get install -y ros-melodic-gscam
+sudo apt-get install -y ros-melodic-opencv-apps
+sudo apt-get install -y ros-melodic-opencv-apps-dbgsym 
+sudo apt-get install -y ros-melodic-vision-opencv
+sudo apt-get install -y ros-melodic-ar-track-alvar 
+sudo apt-get install -y ros-melodic-ar-track-alvar-dbgsym 
+sudo apt-get install -y ros-melodic-ar-track-alvar-msgs
+sudo apt-get install -y ros-melodic-eigen-conversions
+sudo apt-get install -y ros-melodic-eigen-conversions-dbgsym 
+sudo apt-get install -y ros-melodic-eigen-stl-containers 
+sudo apt-get install -y ros-melodic-rgbd-launch  
+
+# Build Turtlebot3 tools:
+sudo apt-get install -y ros-melodic-parrot-arsdk 
+sudo apt-get install -y ros-melodic-turtlebot3-applications-msgs
+sudo apt-get install -y ros-melodic-turtlebot3-fake 
+sudo apt-get install -y ros-melodic-turtlebot3-fake-dbgsym 
+sudo apt-get install -y ros-melodic-turtlebot3-gazebo 
+sudo apt-get install -y ros-melodic-turtlebot3-gazebo-dbgsym 
+sudo apt-get install -y ros-melodic-turtlebot3-msgs
+sudo apt-get install -y  ros-melodic-turtlebot3-simulations
+sudo apt-get install -y ros-melodic-turtlebot3-msgs 
+
+# Stream vídeo packages"
+
+sudo apt-get install -y libgstreamer1.0-dev 
+sudo apt-get install -y libgstreamer-plugins-* 
+sudo apt-get install -y gstreamer1.0-libav* 
+sudo apt-get install -y gstreamer1.0-plugins* 
+sudo apt-get install -y librealsense2-dev 
+sudo apt-get install -y librealsense2-utils 
+# 5. ENVIRONMENT SETUP
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# 6. CREATE AND BUILD A WORKSPACE 
+
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 catkin_init_workspace
 cd ~/catkin_ws
-catkin_make
-echo "O ambiente de trabalho do ROS foi configurado com sucesso"
-echo 'source ~/catkin_ws/devel/setup.bash' >> ~/.bashrc
-sudo apt-get install librealsense2-dev librealsense2-utils ros-melodic-rgbd-launch
-sudo apt install -y ros-melodic-turtlebot3-msgs ros-melodic-ar-track-alvar ros-melodic-turtlebot3-applications-msgs
-echo "A biblioteca que reconhece marcadores de RA foi instalada com sucesso"
+
+# 7. BUILDING OPENCV4 DEPENDECES
+echo "teste opencv4"
+cd ~/catkin_ws/src
+
+git clone https://github.com/ros-perception/image_pipeline.git
+git clone https://github.com/ros-perception/image_transport_plugins.git
+git clone https://github.com/ros-perception/vision_opencv.git
+git clone https://github.com/ros-visualization/rqt_image_view.git
+cd ~/catkin_ws/
+
+catkin_make VERBOSE=1 -j4 -DCMAKE_BUILD_TYPE=Release --cmake-args -DOpenCV_DIR=~/src/opencv4/build/install
+
+echo "instalou o opencv4 no ros"
+
+#8. BUILDING TURTLEBOT3 DEPENDECES
+
 cd ~/catkin_ws/src
 git clone https://github.com/ros-teleop/teleop_twist_keyboard.git    
 git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_applications.git  
 git clone https://github.com/Insper/robot19.git 
 git clone https://github.com/intel-ros/realsense.git
-
 cd ~/catkin_ws/
 catkin_create_pkg gscam raspicam.launch  rospy roscpp
 catkin_make
 mv /home/borg/404/scripts_robotica/raspicam.launch /opt/ros/melodic/share/gscam
 
-echo "os gits para controle do Turtlebot foram clonandos com sucesso dos repositórios da ROBOTIS"
-cd $path
-exec "~/404/scripts_robotica/instalar_opencv_jupyter.sh"							
 
-echo "o opencv no python3 foi instalado, vamos instalar no python2 agora"								cd $path
-exec "~/404/scripts_robotica/instalar_opencv_python2.sh"
-cd $path
-echo "o opencv foi instalado com sucesso no python2 tambem, agora vamos instalar o hector slam"
-exec "~/404/scripts_robotica/instalar_hector_slam.sh"
-
-
-
-
-
-
-
-
-
+echo "-------------------FINISH INSTALL ---------------------"
 
