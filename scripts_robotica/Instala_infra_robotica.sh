0ba42988e0eec97fbe4ff75689a127453f01365b
Licia Sales <40764768+liciascl@users.noreply.github.com>
2020-02-03 15:34:53 -0300

    fix bug

--- scripts_robotica/Instala_infra_robotica.sh
+++ scripts_robotica/Instala_infra_robotica.sh
@@ -12,14 +12,13 @@ echo $path
 # 1. SETUP YOUR SOURCES.LIST AND CONFIGURE THE KEYS
 
 
-echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main' | sudo tee /etc/apt/sources.list.d/realsense-public.list  #Key libRealSense2
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 #Key Ros
 
 
+echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main' | sudo tee /etc/apt/sources.list.d/realsense-public.list  #Key libRealSense2
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 # 2. KEEP UBUNTU OR DEBIAN UP TO DATE
 
