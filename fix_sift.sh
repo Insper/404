ed44c700c31c2949ff4d46319a17b0cf2dffd5f2
Licia Sales <40764768+liciascl@users.noreply.github.com>
2020-02-19 09:04:41 -0300

    Create fix_sift.sh

---
+++ fix_sift.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SECONDS=0
+git reset --hard origin/master
+git pull 
+cd scripts_robotica/OpenCV/build
+sudo make install
+sudo ldconfig
+
+
+echo $ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
