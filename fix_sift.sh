01780735c230e9168aa33fb714914b33b6691ccf
Licia Sales <40764768+liciascl@users.noreply.github.com>
2020-02-19 09:19:52 -0300

    Update fix_sift.sh

--- fix_sift.sh
+++ fix_sift.sh
@@ -4,8 +4,12 @@ SECONDS=0
 git reset --hard origin/master
 git pull 
 cd scripts_robotica/OpenCV/build
+NUM_CPU=$(nproc)
+time make -j$(($NUM_CPU - 1))
 sudo make install
 sudo ldconfig
 
 
 echo $ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+#!/bin/bash
+
