a573fb2cacba117703058db900f88b33f2b1959a
Licia Sales <40764768+liciascl@users.noreply.github.com>
2020-03-09 14:22:02 -0300

    git lfs

--- scripts_robotica/Instala_infra_robotica.sh
+++ scripts_robotica/Instala_infra_robotica.sh
@@ -11,9 +11,9 @@ echo $path
 
 # 0. INSTALL GIT AND GIT LFS
 
-sudo apt intall git
+sudo apt install -y git
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
+sudo apt-get -y  install git-lfs
 git lfs install
 
 
