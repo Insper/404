aca20c6028b51b980328025d3de62253209be9d0
Licia Sales <40764768+liciascl@users.noreply.github.com>
2021-02-12 15:35:43 -0300

    Create elementos.sh

---
+++ scripts_robotica/elementos.sh
@@ -0,0 +1,15 @@
+##########################
+# 3.o Semestre Comp.     #
+##########################
+
+#############
+# Elementos #
+#############
+export ALTERAPATH=$HOME/intelFPGA_lite/20.1
+export PATH=$PATH:${ALTERAPATH}/quartus/bin
+export PATH=$PATH:${ALTERAPATH}/modelsim_ase/linuxaloem/
+export PATH=$PATH:${ALTERAPATH}/modelsim_ase/lib32
+export VUNIT_MODELSIM_PATH=${ALTERAPATH}/modelsim_ase/linuxaloem/
+export VUNIT_SIMULATOR=modelsim
+export QSYS_ROOTDIR="$HOME/intelFPGA_lite/20.1/quartus/sopc_builder/bin"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${ALTERAPATH}/modelsim_ase/lib32
