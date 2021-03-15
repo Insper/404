cb3ad92fa7f5263c0b7758cf6f1adfff33979a30
Licia Sales <40764768+liciascl@users.noreply.github.com>
2021-03-15 13:55:19 -0300

    Create patch_0_anydesk_chrome.sh

--- desktop ssd/patchs/patch_0_anydesk_chrome.sh
+++ desktop ssd/patchs/patch_0_anydesk_chrome.sh
@@ -3,7 +3,7 @@
 # Rafael Corsi
 # path para o SSD Eng Comp 2020-2
 #   - Reinstala anydesk para gerar novo id
-#   - instala google chrome (proctorio)
+
 
 cd tmp/
 
@@ -20,9 +20,3 @@ echo "install anydesk"
 wget https://download.anydesk.com/linux/anydesk_6.0.1-1_amd64.deb -P /tmp/
 sudo apt install -y /tmp/anydesk_6.0.1-1_amd64.deb
 
-#############################################
-##            chrome                      ##
-#############################################
-echo "install google chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp/
-sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
