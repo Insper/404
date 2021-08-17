202a8ed2471a6f8a6fcb33dccbcb8162b4def3c6
Licia Sales <40764768+liciascl@users.noreply.github.com>
2021-08-17 18:38:39 -0300

    Update limpeza_marota.sh

--- scripts_robotica/limpeza_marota.sh
+++ scripts_robotica/limpeza_marota.sh
@@ -3,7 +3,16 @@
 # 26-Jun-19
 
 
+#---------------------------------------update ubuntu ----------------------------------------
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+#sudo gedit /etc/apt/sources.list.d/vscode.list
+#deb [arch=amd64,arm64,armhf] http://packages.microsoft.com/repos/code stable main"
+
 sudo apt install -y bleachbit
 history -c
 bleachbit --clean firefox.*
+bleachbit --clean chrome.*
 sudo rm -rf /etc/NetworkManager/system-connections/*
+git config --global --unset credential.helper
+
