e35904984cee0d74f28a426bcd1fd7fd98417453
Licia Sales <40764768+liciascl@users.noreply.github.com>
2023-03-06 18:35:44 -0300

    Update enableHotSpot.sh

--- scripts_robotica/enableHotSpot.sh
+++ scripts_robotica/enableHotSpot.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 export IFNAME="wlan0"
-export CON_NAME="turtle11"
+export CON_NAME="turtle01"
 export CON_PASS="turtlebot"
+
+
+pkill wpa_supplicant && sudo pkill dhcpcd
+#systemctl restart NetworkManager
 
 rm /etc/NetworkManager/system-connections/$CON_NAME
 nmcli con add type wifi ifname $IFNAME con-name $CON_NAME autoconnect yes ssid $CON_NAME
@@ -10,3 +14,4 @@ nmcli con modify $CON_NAME 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.
 nmcli con modify $CON_NAME wifi-sec.key-mgmt wpa-psk
 nmcli con modify $CON_NAME wifi-sec.psk $CON_PASS
 nmcli con up $CON_NAME
+
