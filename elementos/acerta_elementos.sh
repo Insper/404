9c47810142c70ac0363363f36a668a9d19f1b15f
Rogerio B Cuenca <rogeriobcuenca@gmail.com>
2026-03-11 16:03:04 -0300

    ajuste script elementos

--- elementos/acerta_elementos.sh
+++ elementos/acerta_elementos.sh
@@ -1,4 +1,5 @@
-pip3 install --force-reinstall cocotb==1.8.1
-pip3 install --force-reinstall cocotb-test==0.2.5
-pip3 install --force-reinstall pytest==7.4.4
-pip3 install --force-reinstall pytest-sugar==1.0.0
+pip uninstall -y pytest pluggy pytest-sugar cocotb cocotb-test
+pip install pytest==7.4.4
+pip install cocotb==1.8.1
+pip install cocotb-test==0.2.5
+pip install pytest-sugar==1.0.0
