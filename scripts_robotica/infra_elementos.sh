de37653e141f8092aee5ec4d083973aac1bb7fad
Rogerio B Cuenca <rogeriobcuenca@gmail.com>
2026-03-11 15:26:06 -0300

    infra elementos

---
+++ scripts_robotica/infra_elementos.sh
@@ -0,0 +1,4 @@
+pip3 install --force-reinstall cocotb==1.8.1
+pip3 install --force-reinstall cocotb-test==0.2.5
+pip3 install --force-reinstall pytest==8.3.2
+pip3 install --force-reinstall pytest-sugar==1.0.0
