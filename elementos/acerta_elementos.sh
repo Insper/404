50ca9e5a1f7f348be6bd72455e29d1df155e0e80
Rogerio B Cuenca <rogeriobcuenca@gmail.com>
2026-03-11 15:53:37 -0300

    ajuste script elementos

--- elementos/acerta_elementos.sh
+++ elementos/acerta_elementos.sh
@@ -1,12 +1,4 @@
-echo "reinstalando GHDL.."
-sudo apt install ghdl -y
-echo "reinstalando pytest.."
-pip3 install --force-reinstal pytest==8.3.2
-echo "reinstalando dependencias" 
-pip3 install --force-reinstall pytest-sugar==1.0.0
 pip3 install --force-reinstall cocotb==1.8.1
 pip3 install --force-reinstall cocotb-test==0.2.5
-echo "atualizando elementos.sh"
-cd
-source .bashrc
-echo "resolvido!!!"
+pip3 install --force-reinstall pytest==7.4.4
+pip3 install --force-reinstall pytest-sugar==1.0.0
